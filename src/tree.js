4f8281ad7361451d9b572e8c1f4a039b359ce353
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-11 14:23:14 +0300

    Move mergeSort into separate module

--- src/tree.js
+++ src/tree.js
@@ -1,40 +1,5 @@
+import { mergeSort } from "./mergeSort";
+
 export class Tree {
   constructor() {}
-}
-
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
-
-  const midIdx = Math.round(arr.length / 2);
-
-  const left = arr.slice(0, midIdx);
-  const right = arr.slice(midIdx);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(leftArr, rightArr) {
-  const merged = [];
-
-  const m = leftArr.length;
-  const n = rightArr.length;
-
-  let i = 0,
-    j = 0;
-
-  while (i < m && j < n) {
-    if (leftArr[i] < rightArr[j]) {
-      merged.push(leftArr[i++]);
-    } else {
-      merged.push(rightArr[j++]);
-    }
-  }
-  for (; i < m; i++) {
-    merged.push(leftArr[i]);
-  }
-  for (; j < n; j++) {
-    merged.push(rightArr[j]);
-  }
-
-  return merged;
 }
