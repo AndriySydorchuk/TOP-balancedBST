d16e5f9763f9278a4c0954ee9dec5879b0281aa6
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-11 16:10:53 +0300

    Add buildTree method to build tree using array

--- src/tree.js
+++ src/tree.js
@@ -1,5 +1,28 @@
-import { mergeSort } from "./mergeSort";
+import { mergeSort } from "./mergeSort.js";
+import { Node } from "./node.js";
 
 export class Tree {
-  constructor() {}
+  #root;
+
+  constructor(arr) {
+    const normalizedArr = this.#normalizeArr(arr);
+
+    this.#root = this.#buildTree(normalizedArr, 0, normalizedArr.length - 1);
+  }
+
+  #normalizeArr(arr) {
+    return mergeSort([...new Set(arr)]);
+  }
+
+  #buildTree(arr, start, end) {
+    if (start > end) return null;
+
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.#buildTree(arr, start, mid - 1);
+    root.right = this.#buildTree(arr, mid + 1, end);
+
+    return root;
+  }
 }
